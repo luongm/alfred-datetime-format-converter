5219df38e6eb2c23fd7d53b447338d349eafb781
Minh Luong <en14vn@gmail.com>
2014-07-23 12:41:15 -0700

    Add local time zone and handles milliseconds

--- workflow/process.py
+++ workflow/process.py
@@ -2,6 +2,7 @@
 
 import alfred
 import calendar
+import datetime, time
 from delorean import utcnow, parse, epoch
 
 def process(query_str):
@@ -16,12 +17,15 @@ def parse_query_value(query_str):
     """ Return value for the query string """
     try:
         query_str = str(query_str).strip('"\' ')
-        if query_str == 'now':
+        if query_str == "" or query_str == 'now':
             d = utcnow()
         else:
             # Parse datetime string or timestamp
             try:
-                d = epoch(float(query_str))
+                if len(query_str) == 13:
+                    d = epoch(float(query_str)/1000) # if query string is in ms
+                else:
+                    d = epoch(float(query_str))
             except ValueError:
                 d = parse(str(query_str))
     except (TypeError, ValueError):
@@ -35,46 +39,50 @@ def alfred_items_for_value(value):
     """
 
     index = 0
+    timestring_format = "%a, %Y-%m-%d %H:%M:%S"
+    dt_object = value.datetime
     results = []
 
     # First item as timestamp
-    item_value = calendar.timegm(value.datetime.utctimetuple())
+    item_value = calendar.timegm(dt_object.utctimetuple())
     results.append(alfred.Item(
         title=str(item_value),
-        subtitle=u'UTC Timestamp',
+        subtitle=u'Epoch Timestamp (sec)',
         attributes={
-            'uid': alfred.uid(index), 
+            'uid': alfred.uid(index),
             'arg': item_value,
         },
         icon='icon.png',
     ))
     index += 1
 
-    # Various formats
-    formats = [
-        # 1937-01-01 12:00:27
-        ("%Y-%m-%d %H:%M:%S", ''),
-        # 19 May 2002 15:21:36
-        ("%d %b %Y %H:%M:%S", ''), 
-        # Sun, 19 May 2002 15:21:36
-        ("%a, %d %b %Y %H:%M:%S", ''), 
-        # 1937-01-01T12:00:27
-        ("%Y-%m-%dT%H:%M:%S", ''),
-        # 1996-12-19T16:39:57-0800
-        ("%Y-%m-%dT%H:%M:%S%z", ''),
-    ]
-    for format, description in formats:
-        item_value = value.datetime.strftime(format)
-        results.append(alfred.Item(
-            title=str(item_value),
-            subtitle=description,
-            attributes={
-                'uid': alfred.uid(index), 
-                'arg': item_value,
-            },
+    # Second item as GMT timestamp
+    item_value = dt_object.strftime(timestring_format)
+    results.append(alfred.Item(
+        title=str(item_value),
+        subtitle='GMT timestamp',
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': item_value,
+        },
+    icon='icon.png',
+    ))
+    index += 1
+
+    # Third item as local timestamp (most likely PST)
+    now = int(time.time())
+    offset = datetime.datetime.fromtimestamp(now) - datetime.datetime.utcfromtimestamp(now)
+    item_value = datetime.datetime.utcfromtimestamp(calendar.timegm(dt_object.utctimetuple())) + offset
+    results.append(alfred.Item(
+        title=item_value.strftime(timestring_format),
+        subtitle=u'{} timestamp'.format(time.tzname[0]),
+        attributes={
+            'uid': alfred.uid(index),
+            'arg': item_value,
+        },
         icon='icon.png',
-        ))
-        index += 1
+    ))
+    index += 1
 
     return results
 
